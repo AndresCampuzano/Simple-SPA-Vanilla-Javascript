416a3be872b4b478f448abd74d20ccdee4fcb42f
Andres Campuzano <campuxano88@gmail.com>
2020-05-06 00:14:20 -0500

    Web done.

---
+++ js/routes.js
@@ -0,0 +1,14 @@
+const routes = [
+	{
+		path: '/',
+		template: '<h1>This is the Home</h1>',
+	},
+	{
+		path: '/contact',
+		template: '<h1>This is Contact Page</h1>',
+	},
+	{
+		path: '/aboutme',
+		template: '<h1>This is About me Page</h1>',
+	},
+];
